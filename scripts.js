0013ad095040d58823c07330d42f2d11c682b8a5
Mitch Watson <wats0314@d.umn.edu>
2023-03-26 17:34:37 -0500

    hid irrelevant CSS buttons depending on active stylesheet

--- scripts.js
+++ scripts.js
@@ -2,3 +2,8 @@ function changeSheet(){
 event.preventDefault();
 document.getElementById("styler").setAttribute("href","styles.css");
 console.log("run")}
+
+function changeBack(){
+  event.preventDefault();
+  document.getElementById("styler").setAttribute("href","styles2.css");
+  console.log("run")}
